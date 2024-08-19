076ca03ff08f23129f97391e382bab4dc1ac939c
Robert Chatterton <robbie.chatterton@gmail.com>
2024-08-19 13:24:23 -0400

    add shows

--- src/cards/UpcomingShowCard.tsx
+++ src/cards/UpcomingShowCard.tsx
@@ -9,42 +9,21 @@ export default function UpcomingShowCard() {
           linkUrl='https://www.ivymanor.com/'
           linkFormat='Ivy Manor Inn'
           location='Bar Harbor, ME'
-          date='08.03.24'
-          subtitle='Show from 2PM - 5PM'
-        />
-        <UpcomingShow
-          linkUrl='https://www.ivymanor.com/'
-          linkFormat='Ivy Manor Inn'
-          location='Bar Harbor, ME'
-          date='08.08.24'
-          subtitle='Show from 2PM - 5PM'
-        />
-        <UpcomingShow
-          linkUrl='https://www.facebook.com/p/Black-Moon-Public-House-61557620630623/'
-          linkFormat='Black Moon Public House'
-          location='Ellsworth, ME'
-          date='08.10.24'
-          subtitle='Show from 6PM - 8PM'
-        />
-        <UpcomingShow
-          linkUrl='https://www.ivymanor.com/'
-          linkFormat='Ivy Manor Inn'
-          location='Bar Harbor, ME'
-          date='08.15.24'
-          subtitle='Show from 2PM - 5PM'
-        />
-        <UpcomingShow
-          linkUrl='https://www.mofga.org/'
-          linkFormat='MOFGA Benefit'
-          location='Lamoine, ME'
-          date='08.17.24'
-          subtitle='Show from 6PM - 7:30PM'
-        />
-        <UpcomingShow
-          linkUrl='https://www.ivymanor.com/'
-          linkFormat='Ivy Manor Inn'
-          location='Bar Harbor, ME'
           date='08.22.24'
+          subtitle='Show from 2PM - 5PM'
+        />
+        <UpcomingShow
+          linkUrl='https://www.ivymanor.com/'
+          linkFormat='Ivy Manor Inn'
+          location='Bar Harbor, ME'
+          date='08.23.24'
+          subtitle='Show from 2PM - 5PM'
+        />
+        <UpcomingShow
+          linkUrl='https://www.ivymanor.com/'
+          linkFormat='Ivy Manor Inn'
+          location='Bar Harbor, ME'
+          date='08.29.24'
           subtitle='Show from 2PM - 5PM'
         />
         <UpcomingShow
