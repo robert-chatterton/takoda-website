1163df3eb8ab34fd7e39b0fa97e3d93c4fdfa836
Robert Chatterton <robbie.chatterton@gmail.com>
2024-08-28 08:58:17 -0400

    updated dates

--- src/cards/UpcomingShowCard.tsx
+++ src/cards/UpcomingShowCard.tsx
@@ -9,20 +9,6 @@ export default function UpcomingShowCard() {
           linkUrl='https://www.ivymanor.com/'
           linkFormat='Ivy Manor Inn'
           location='Bar Harbor, ME'
-          date='08.22.24'
-          subtitle='Show from 2PM - 5PM'
-        />
-        <UpcomingShow
-          linkUrl='https://www.ivymanor.com/'
-          linkFormat='Ivy Manor Inn'
-          location='Bar Harbor, ME'
-          date='08.23.24'
-          subtitle='Show from 2PM - 5PM'
-        />
-        <UpcomingShow
-          linkUrl='https://www.ivymanor.com/'
-          linkFormat='Ivy Manor Inn'
-          location='Bar Harbor, ME'
           date='08.29.24'
           subtitle='Show from 2PM - 5PM'
         />
@@ -32,6 +18,13 @@ export default function UpcomingShowCard() {
           location='Ellsworth, ME'
           date='08.31.24'
           subtitle='Show from 6PM - 8PM'
+        />
+        <UpcomingShow
+          linkUrl='https://www.ivymanor.com/'
+          linkFormat='Ivy Manor Inn'
+          location='Bar Harbor, ME'
+          date='09.02.24'
+          subtitle='Show from 12PM - 2:30PM'
         />
         <UpcomingShow
           linkUrl='https://www.facebook.com/TheAnnexBH/'
@@ -46,6 +39,20 @@ export default function UpcomingShowCard() {
           location='Orono, ME'
           date='09.06.24'
           subtitle='Show from 6PM - 8PM'
+        />
+        <UpcomingShow
+          linkUrl='https://www.ivymanor.com/'
+          linkFormat='Ivy Manor Inn'
+          location='Bar Harbor, ME'
+          date='09.07.24'
+          subtitle='Show from 2PM - 5PM'
+        />
+        <UpcomingShow
+          linkUrl='https://www.ivymanor.com/'
+          linkFormat='Ivy Manor Inn'
+          location='Bar Harbor, ME'
+          date='09.12.24'
+          subtitle='Show from 2PM - 5PM'
         />
         <UpcomingShow
           linkUrl='https://www.facebook.com/TheAnnexBH/'
@@ -67,6 +74,13 @@ export default function UpcomingShowCard() {
           location='Bar Harbor, ME'
           date='10.16.24'
           subtitle='Show from 8PM - 10PM'
+        />
+        <UpcomingShow
+          linkUrl='https://www.barharbormusicfestival.org/'
+          linkFormat='Bar Harbor Music Festival'
+          location='Bar Harbor, ME'
+          date='10.20.24'
+          subtitle='Show from 2PM - 5PM @ the Ivy Manor Inn'
         />
         <UpcomingShow
           linkUrl='https://maps.app.goo.gl/NJzqocr9J3DXrVcJA'
