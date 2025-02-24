b4214ead8eb2fb57889b4eb51e7b0eb3b9855949
Robert Chatterton <robbie.chatterton@gmail.com>
2025-02-24 16:59:02 -0500

    update bio

--- src/cards/AboutMeCard.tsx
+++ src/cards/AboutMeCard.tsx
@@ -9,21 +9,24 @@ export default function AboutMeCard() {
       </div>
       <div className='font-thin'>
         <p>
-          Takoda Dionne is a singer-songwriter born and raised on the coast of
-          Maine. He currently lives in Bar Harbor. Takoda started playing music
-          at a young age and writing his own music in middle school. His musical
-          journey started with the flute but quickly changed to guitar but he
-          also plays bass, drums and keyboard. He has a passion for guitars and
-          can be seen playing at local venues year round.
+          Takoda Dionne is a live looping singer-songwriter born and raised on
+          the coast of Maine. He is currently based in Bar Harbor. Takoda
+          started playing music at a young age and writing his own music in
+          middle school. His musical journey began with the flute but guitar
+          quickly became his instrument of choice. He also plays bass, drums and
+          keyboard/piano. He has a passion for guitars and can be seen playing
+          at local venues year round.
         </p>
         <GalleryMedia src={require('../assets/gallery/DSC_0272.png')} />
         <p>
-          Some of his influences include: Ed Sheeran, Shawn Mendes, Passenger,
-          Chris Cornell, Noah Kahan and Pearl Jam. Takoda plays an eclectic
-          selection of covers and his original music is a thoughtful and
-          passionate blend of pop, rock, and folk music. Takoda layers acoustic
-          guitar, percussion, and bass using a single guitar with live looping
-          and passionate vocals often including layered harmonies.
+          Takoda plays an eclectic selection of covers and originals, both live
+          looped and acoustic. His original music is a thoughtful and passionate
+          blend of pop, rock, and folk music. Using live looping Takoda layers
+          acoustic guitar, percussion, and bass using a single guitar and
+          sometimes piano or electric guitar. He adds his passionate vocals that
+          often include layered vocal harmonies using a mic connected to his
+          looper which creates the illusion of a multi-instrumental band with
+          multiple singers. His music is always live with nothing pre-recorded.
         </p>
       </div>
       <GalleryMedia src={require('../assets/gallery/guitar.png')} />
